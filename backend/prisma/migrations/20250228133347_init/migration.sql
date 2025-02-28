a54dc50333251927da067941d6b9b6f99fb04e2f
LucasNGRE <lucas12negre@gmail.comgit config --global user.email lucas12negre@gmail.com>
2025-02-28 14:35:29 +0100

    Ajout du backend

---
+++ backend/prisma/migrations/20250228133347_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Lure" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "Lure_pkey" PRIMARY KEY ("id")
+);
